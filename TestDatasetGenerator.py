b2518035d4434bb7cf80f24180d3b30bc884e691
johannathiemich <johanna.thiemich@gmx.de>
2021-03-20 16:59:26 +0100

    Added documentation

--- TestDatasetGenerator.py
+++ TestDatasetGenerator.py
@@ -7,12 +7,28 @@ import math
 import pickle
 import os
 
+
+"""
+This class is used for creating random numpy arrays where the size of one dimension follows one of 5 possible distributions
+(Gaussian, Exponential_Right, Exponential_Left, Random, Equal)
+Also the numpy arrays are saved to files on the disk.
+This was used to create dummy data for evaluating the data samplers.
+"""
 class TestDatasetGenerator:
     def __init__(self, num_items=100, distribution="Gaussian", min_length=2, max_length=60, train_events=1, feature_dim=4, root_dir="./"):
+        """
+        num_items: number of numpy files to create
+        distribution: the distribution that the length of the drawn samples will follow
+        min_length: minimum size of varying dimension
+        max_length: maximum size of varying dimension
+        train_events: number of arrays per numpy file
+        root_dir: where to save the numpy array files to
+        """
         self.num_items = num_items
         self.data = []
         self.labels = []
         self.distribution = distribution if distribution in ["Gaussian", "Exponential_Right", "Exponential_Left", "Random", "Equal"] else "Random"
+        #checking whether chosen min and max lengths make sense
         if min_length < max_length:
             self.min_length = min_length
             self.max_length = max_length
@@ -26,9 +42,11 @@ class TestDatasetGenerator:
         if os.path.isdir(root_dir):
             self.root_dir = root_dir
             if not os.path.isdir(os.path.join(self.root_dir, self.distribution)):
+                #creating the directory to save the files to if it does not exist yet
                 os.mkdir(os.path.join(self.root_dir, self.distribution))
             self.save_dir = os.path.join(self.root_dir, self.distribution)
-
+        
+        # determining the distibution to use
         if self.distribution == "Gaussian":
             self.create_gaussian()
         elif self.distribution == "Random":
@@ -39,9 +57,16 @@ class TestDatasetGenerator:
             self.create_exponential_right()
         elif self.distribution == "Exponential_Left":
             self.create_exponential_left()
+        else: 
+            #if distribution does not match any of the previous ones, use Gaussian
+            print("The chosen data distribution is invalid. Using Gaussian distribution instead.")
+            self.create_gaussian()
 
         assert len(self.length_list) == self.num_items
         self.save_length_list()
+        
+        #these lines can be uncommented if you want to create numpy files from a length list (list that contains the different lengths
+        #of the arrays)
         #self.length_list = []
         #self.read_length_list(path="/media/johanna/Volume/Studium/Semester_4/Datamanagement/data/Gaussian_1000/length_list_file")
         #print(self.length_list)
@@ -50,6 +75,10 @@ class TestDatasetGenerator:
         self.create_tensors()
 
     def create_gaussian(self):
+        """
+        This function creates a list of lengths, the lengths are drawn from a Gaussian distribution.
+        Samples are drawn until we have enough samples whose length is within [min_range, max_range]
+        """
         center = (int)((self.max_length - self.min_length) / 2) + self.min_length
         scale = math.sqrt(((self.max_length - self.min_length) / 2)) + 4
 
@@ -57,6 +86,7 @@ class TestDatasetGenerator:
         length_list = []
         for _ in range(self.num_items):
             a = np.random.default_rng().normal(center, scale, 1)
+            # if drawn length is smaller than desired min length or greater than max length draw the sample again
             while a < self.min_length or a > self.max_length + 1:
                 a = random.gauss(center, scale)
             length_list.append(int(a))
@@ -66,6 +96,11 @@ class TestDatasetGenerator:
         self.length_list = length_list
 
     def create_exponential_right(self):
+        """
+        This function creates a list of lengths, the lengths are drawn from a Exponential Right distribution.
+        That is a distribution where most lengths are large, and only few are small.
+        Samples are drawn until we have enough samples whose length is within [min_range, max_range]
+        """
         scale = int((self.max_length - self.min_length) / 2) - 4
 
         length_list = []
@@ -81,6 +116,11 @@ class TestDatasetGenerator:
         self.length_list = length_list
 
     def create_exponential_left(self):
+        """
+        This function creates a list of lengths, the lengths are drawn from a Exponential Left distribution.
+        That is a distribution where most lengths are small, and only few are large.
+        Samples are drawn until we have enough samples whose length is within [min_range, max_range]
+        """
         scale = int((self.max_length - self.min_length) / 2) - 4
 
         length_list = []
@@ -95,6 +135,11 @@ class TestDatasetGenerator:
         self.length_list = length_list
 
     def create_equal(self):
+        """
+        This function creates a list of lengths, the lengths are drawn from an Equal distribution.
+        That is a distribution where all lengths are the same.
+        Samples are drawn until we have enough samples whose length is within [min_range, max_range]
+        """
         length_list = []
         for i in range(0, self.num_items):
             length_list.append((i % (self.max_length + 1 - self.min_length)) + self.min_length)
@@ -104,6 +149,10 @@ class TestDatasetGenerator:
         self.length_list = length_list
 
     def create_random(self):
+        """
+        This function creates a list of lengths, the lengths are drawn from a Random distribution.
+        Samples are drawn until we have enough samples whose length is within [min_range, max_range]
+        """
         length_list = []
         for i in range(self.num_items):
             length_list.append(random.randint(self.min_length, self.max_length))
@@ -113,6 +162,9 @@ class TestDatasetGenerator:
         self.length_list = length_list
 
     def visualize_distribution(self, verbose=True):
+        """
+        This function visualizes the distribution of the drawn lengths. 
+        """
         plt.xlim([0, 65])
         bins = len(set(self.length_list))
         plt.hist(self.length_list, bins = len(set(self.length_list)), alpha=0.5)
@@ -124,6 +176,10 @@ class TestDatasetGenerator:
             plt.show()
 
     def create_tensors(self):
+        """
+        This function creates the random numpy arrays, according to the length list.
+        It also saves the created arrays to the disk.
+        """
         lca_matrices = []
         leaves_list = []
 
@@ -151,11 +207,24 @@ class TestDatasetGenerator:
             self._save_to_drive(leaves_list, "leaves", mode, counter)
 
     def _save_to_drive(self, tensor_list, identifier, mode, counter, path="./"):
+        """
+        This function saves a list of tensors to the disk.
+        tensor_list: List of tensors to save to disk
+        identifier: leaves or lcas
+        mode: train or validation
+        counter: number identifying the numpy file
+        path: directory to save numpy files to
+        """
         save_path = self.save_dir if os.path.isdir(self.root_dir) else path
         for tensor in tensor_list:
             np.save(os.path.join(save_path, f"{identifier}_{mode}.{counter}.npy"), tensor)
-
+    
     def save_length_list(self, path="./"):
+        """
+        This function saves the drawn length list to a file so that it is possible
+        to create the numpy arrays or the distribution visualization again just using 
+        this file.
+        """
         save_path = self.save_dir if os.path.isdir(self.root_dir) else path
         save_path = os.path.join(save_path, "length_list_file")
         with open(save_path, "wb") as fp:
@@ -168,7 +237,9 @@ class TestDatasetGenerator:
 
 
 def main():
-    root = "/media/johanna/Volume/Studium/Semester_4/Datamanagement/data/benchmark_10k"
+    root = "/directory/to/save/numpy/files/in"
+    
+    #example usage for saving numpy files:
     test = TestDatasetGenerator (num_items=10000, root_dir=root, distribution="Gaussian", min_length=2, max_length=60)
     test = TestDatasetGenerator (num_items=10000, root_dir=root, distribution="Exponential_Right", min_length=2, max_length=60)
     test = TestDatasetGenerator (num_items=10000, root_dir=root, distribution="Exponential_Left", min_length=2, max_length=60)
